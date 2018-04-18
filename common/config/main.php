91f614860b8b66029d9b9a255a7c69bfc1b7e9ee
DoubleJack <451306164@qq.com>
2018-04-18 19:55:03 +0800

    修改配置文件 common config

--- common/config/main.php
+++ common/config/main.php
@@ -7,7 +7,96 @@ return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
         'cache' => [
-            'class' => 'yii\caching\FileCache',
+            'class' => 'yii\redis\Cache',
         ],
+
+        //redis
+        'redis' => [
+            'class' => 'yii\redis\Connection',
+            'hostname' => 'xxx.redis.rds.aliyuncs.com',
+            'password' => '',
+            'port' => 6379,
+            'database' => 0, //默认
+        ],
+        //统计专用redis
+        'redis-stat' => [
+            'class' => 'yii\redis\Connection',
+            'hostname' => 'xxx.redis.rds.aliyuncs.com',
+            'password' => '',
+            'port' => 6379,
+            'database' => 1,
+        ],
+        //微信公众号
+        'wechat' => [
+            'class' => 'common\extensions\wechat\Wechat',
+            'token' => '',
+            'encoding_aes_key' => '',
+            'appid' => '',
+            'appsecret' => '',
+            'requestHandler' => 'common\services\wechat\RequestHandler',
+        ],
+        //微信小程序
+        'weapp' => [
+            'class' => 'common\extensions\weapp\Weapp',
+            'token' => '',
+            'appid' => '',
+            'appsecret' => '',
+        ],
+
+        //微信redis
+        'redis-wechat' => [
+            'class' => 'yii\redis\Connection',
+            'hostname' => 'xxx.redis.rds.aliyuncs.com',
+            'password' => '',
+            'port' => 6379,
+            'database' => 2, //微信公众号
+        ],
+
+        'cache-redis-wechat' => [
+            'class' => 'yii\redis\Cache',
+            'redis' => 'redis-wechat',
+        ],
+
+        //微信redis
+        'redis-weapp' => [
+            'class' => 'yii\redis\Connection',
+            'hostname' => 'xxx.redis.rds.aliyuncs.com',
+            'password' => '',
+            'port' => 6379,
+            'database' => 3, //微信小程序
+        ],
+
+        'cache-redis-weapp' => [
+            'class' => 'yii\redis\Cache',
+            'redis' => 'redis-weapp',
+        ],
+
+
+        //公众号 -- 微信支付
+        'wechatPay' => [
+            'class' => '\common\extensions\wechatpay\WechatPay',
+            'appid' => '',
+            'mch_id' => '',
+            'key' => '',
+            'notify_url' => 'https://wechat.xxx.com/wechat/pay/notify',
+        ],
+
+        //小程序 -- 微信支付
+        'weappPay' => [
+            'class' => '\common\extensions\weapppay\WeappPay',
+            'appid' => '',
+            'mch_id' => '',
+            'key' => '',
+            'notify_url' => 'https://weapp.xxx.com/weapp/pay/notify',
+        ],
+
+        //阿里云oss
+        'aliyunOss' => [
+            'class' => 'common\extensions\aliyunOss\AliyunOssExtension',
+            'accessKeyId' => '',
+            'accessKeySecret' => '',
+            'endpoint' => '',
+        ],
+        
     ],
 ];
--- common/config/params.php
+++ common/config/params.php
@@ -1,6 +1,25 @@
 <?php
 return [
-    'adminEmail' => 'admin@example.com',
-    'supportEmail' => 'support@example.com',
+    'adminEmail' => 'shuangjiezeng@gmail.com',
+    'supportEmail' => 'shuangjiezeng@gmail.com',
     'user.passwordResetTokenExpire' => 3600,
+
+    //域名
+    'domain.mobile' => 'm.xxx.com',
+    'domain.wechat' => 'wechat.xxx.com',
+    'domain.weapp' => 'weapp.xxx.com',
+    'domain.media' => 'media.xxx.com',
+
+    //OSS bucket
+    'bucket.media' => 'ls-media',
+    'bucket.media_output' => 'ls-media-output',
+
+    //access-token缓存过期时间
+    'access.token.expires.in' => (3600 * 24),
+
+    //模板id
+    'wxTemplateId.pay.notify' => 'xxxx-xxxx',
+    'weappTemplateId.pay.notify' => 'xxxx-xxxx',
+    'wxTemplateId.new.comment.notify' => 'xxxx-xxxx',
+    'weappTemplateId.new.comment.notify' => 'xxxx-xxxx',
 ];
